fec296dc9813eaebfca8024c7f0d10329371ac83
Ryder Wishart <ryderwishart@gmail.com>
2020-01-31 11:02:07 -0500

    Added Google Analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-135700486-10`,
       },
     },
     `gatsby-plugin-feed`,
