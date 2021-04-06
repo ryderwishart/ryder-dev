f28410dd7d2d3870eafd67eb1bab733436b1a396
ryderwishart <ryderwishart@gmail.com>
2021-04-06 11:19:55 -0600

    Hide Tina sidebar

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,16 +10,16 @@ module.exports = {
     },
   },
   plugins: [ 
-    {
-      resolve: "gatsby-plugin-tinacms",
-      options: {
-        plugins: ["gatsby-tinacms-git", "gatsby-tinacms-remark", "gatsby-tinacms-json",],
-        sidebar: {
-          hidden: process.env.NODE_ENV === "production",
-          position: "displace"
-        },
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-tinacms",
+    //   options: {
+    //     plugins: ["gatsby-tinacms-git", "gatsby-tinacms-remark", "gatsby-tinacms-json",],
+    //     sidebar: {
+    //       hidden: process.env.NODE_ENV === "production",
+    //       position: "displace"
+    //     },
+    //   },
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
